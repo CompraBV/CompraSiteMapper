6b4c5e14933d299869c54a03ac76942dc26cbb4a
bobdesaunois <bobdesaunois@gmail.com>
2015-11-20 16:15:34 +0100

    SiteMapGenerator package

---
+++ src/nl/compra/SiteMapGenerator/Program.java
@@ -0,0 +1,19 @@
+package nl.compra.SiteMapGenerator;
+
+public class Program {
+
+	public Program ()
+	{
+		
+		System.out.println ("This will eventually be a SiteMapGenerator.");
+		
+	}
+	
+	public static void main (String[] args)
+	{
+		
+		new Program ();
+		
+	}
+	
+}
