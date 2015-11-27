2df96c2cc237467662367241a02eb39988a81739
bobdesaunois <bobdesaunois@gmail.com>
2015-11-27 15:51:51 +0100

    Ridding unused imports

--- src/nl/compra/CompraCrawler/Crawler.java
+++ src/nl/compra/CompraCrawler/Crawler.java
@@ -6,11 +6,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 
 
